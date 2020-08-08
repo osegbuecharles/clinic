a6f9be0f17cbee20b495551eeab04ae90c584e74
Osegbue Charles <chukacj@hotmail.com>
2020-08-08 16:27:20 +0100

    Update index.php

--- API/setClinicData/index.php
+++ API/setClinicData/index.php
@@ -36,7 +36,7 @@
                                 echo json_encode(array("error"=>FALSE,"data"=>array("success"=>TRUE)));
                             }
                             else{
-                                echo json_encode(msqli_error($link));
+                                echo json_encode(array("error"=>TRUE,"message"=>msqli_error($link)));
                             }
                         }
                         else{
